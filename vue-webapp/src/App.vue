d43bc7ab578649683067285dfb09af5f3e1ceb75
NEAWX2 <s67319010026@kktech.ac.th>
2024-10-22 13:52:00 +0700

    22/10/67

---
+++ vue-webapp/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import mybar from './views/mybar.vue';
+import Mycard from './views/Mycard.vue';
+</script>
+
+<template>
+  <header>
+    <mybar />
+  </header>
+
+  <main>
+    <Mycard />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-webapp/src/views/Mycard.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Mycard</h1>
+</template>
